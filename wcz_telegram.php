a221a46aff6620c8f3226621d41ad5e4af662776
wcz-txp <2091265+wcz-txp@users.noreply.github.com>
2020-05-05 11:24:54 +0300

    Some minor changes eg. sorting keywords and convert textile to markdown. Added utm-parameter for web analytics.

--- wcz_telegram.php
+++ wcz_telegram.php
@@ -1,10 +1,3 @@
-<?php
-
-/*
-Partly inspired by Andy Carter's arc_twitter
-http://andy-carter.com/txp/arc_twitter
-*/
-
 if (txpinterface == 'admin')
 {
     add_privs('wcz_telegram_post', '1');
@@ -38,99 +31,113 @@ function wcz_telegram() {
 
     $article_id = empty($GLOBALS['ID']) ? gps('ID') : $GLOBALS['ID'];
     $Status = empty($GLOBALS['Status']) ? gps('Status') : $GLOBALS['Status'];
-    $view = empty($GLOBALS['view']) ? gps('view') : $GLOBALS['view'];
     $wcz_telegram__this = empty($GLOBALS['wcz_telegram__this']) ? gps('wcz_telegram__this') : $GLOBALS['wcz_telegram__this'];
 
-    if ($Status == 4 and $wcz_telegram__this == 1 and $view != "html" and $view != "preview")
+    if ($Status == 4 and $wcz_telegram__this == 1)
     {
 
 
-	if (!empty($article_id)) 
-	{
+    if (!empty($article_id)) 
+    {
 
-	    include_once txpath.'/publish/taghandlers.php';
+        include_once txpath.'/publish/taghandlers.php';
 
-	    $article = safe_row("ID, Posted, LastMod, Title, Excerpt, Keywords, Status", 'textpattern',"ID={$article_id}");
-	}
+        $article = safe_row("ID, Posted, LastMod, Title, Excerpt, Keywords, Status", 'textpattern',"ID={$article_id}");
+    }
 
 
-	$article_url = permlinkurl($article);
-	$title = $article['Title'];
-	$excerpt = $article['Excerpt'];
-	$keywords = $article['Keywords'];
+    $article_url = permlinkurl($article);
+    $title = $article['Title'];
+    $excerpt = $article['Excerpt'];
+    $keywords = $article['Keywords'];
 
 // the token of your Telegram bot
-	$wcz_telegram_token = '';
+    $wcz_telegram_token = '';
 
 // the chatid of your Telegram channel
-	$wcz_telegram_chatid = '';
+    $wcz_telegram_chatid = '';
 
 // If you have already a Telegram Instant View template (https://instantview.telegram.org/#publishing-templates), but this is not really public
 // you should consider to use this option. Set it to '1' and copy the rhash, so Telegram users will see your article on their mobiles
 // inside Telegram rendered with your template.
-	$wcz_telegram_iv = '0';
-	$wcz_telegram_rhash = '';
+    $wcz_telegram_iv = '0';
+    $wcz_telegram_rhash = '';
+
+// give traffic analytics some stuff "&utm_source=tg&utm_medium=social"
+    $wcz_telegram_utm = '0';
 
 // If something doesn't work, switch it to '1' and you'll get the complete request inside of textpattern/tmp/telegram_request.txt
-	$wcz_telegram_debug = '0';
+    $wcz_telegram_debug = '0';
 
-	if ($wcz_telegram_iv == '1') 
-	{
-	    $article_url = "https://t.me/iv?url=".$article_url."&rhash=".$wcz_telegram_rhash;
-	}
+    if ($wcz_telegram_iv == '1') 
+    {
+        $article_url = "https://t.me/iv?url=".$article_url."&rhash=".$wcz_telegram_rhash;
+    } 
 
+    if ($wcz_telegram_utm == '1') 
+    {
+            if( strpos( $article_url, '?' ) !== false) {
+                  $article_url = $article_url."&utm_source=tg&utm_medium=social";
+            } else {
+              $article_url = $article_url."?utm_source=tg&utm_medium=social";
+             }
+    }
 
 //  escape Markdown formatting character "_"
-	$article_url = str_replace("_","\_",$article_url);
+    $article_url = str_replace("_","\_",$article_url);
+
+//  convert Textile "??" to Markdown formatting character "_"
+        $excerpt = str_replace("??","_",$excerpt);
 
 //  prepare keywords
-	$keywords = explode(",",$keywords);
-	foreach ($keywords as &$keyword) 
-	{
-	    $keyword = preg_replace('/\s+/', '',mb_convert_case($keyword, MB_CASE_TITLE));
-	}
+    $keywords = explode(",",$keywords);
+    foreach ($keywords as &$keyword) 
+    {
+        $keyword = preg_replace('/[\s_-]+/', '',mb_convert_case($keyword, MB_CASE_TITLE));
+    }
 
-	$keywords = implode(" ",preg_filter('/^/', '#', $keywords));
+        sort($keywords);
+
+    $keywords = implode(" ",preg_filter('/^/', '#', $keywords));
 
 // 4096 characters is the maximum post length
 
-	$excerptlen = 4080 - strlen($title) - strlen($keywords) - strlen($article_url);
+    $excerptlen = 4080 - strlen($title) - strlen($keywords) - strlen($article_url);
 
-	if (strlen($excerpt) > $excerptlen) 
-	{
-	    $pos=strpos($excerpt, ' ', $excerptlen);
-	    $excerpt = '"'.substr($excerpt,0,$pos).'" ...';
-	}
+    if (strlen($excerpt) > $excerptlen) 
+    {
+        $pos=strpos($excerpt, ' ', $excerptlen);
+        $excerpt = '"'.substr($excerpt,0,$pos).'" ...';
+    }
 
-	$message = "*".$title."*\n\n".$excerpt."\n\n".$keywords."\n\n".$article_url;
+        $message = preg_replace('/\?\?/', '__',$message);
 
-	$url = 'https://api.telegram.org/bot'.$wcz_telegram_token.'/sendMessage?chat_id='.$wcz_telegram_chatid.'&parse_mode=Markdown&text=';
-	$url .= urlencode($message);
+    $message = "*".$title."*\n\n".$excerpt."\n\n".$keywords."\n\n".$article_url;
+
+    $url = 'https://api.telegram.org/bot'.$wcz_telegram_token.'/sendMessage?chat_id='.$wcz_telegram_chatid.'&parse_mode=Markdown&text=';
+    $url .= urlencode($message);
 
 
 // some debug stuff
-	if ($wcz_telegram_debug == "1") 
-	{
-	    file_put_contents(txpath."/tmp/telegram_request.txt","Sent URL: \n".$url."\n\nPrepared message:\n".$message."\n\nExcerpt length:\n".$excerptlen."\n\nCurl output:\n");
-	}
+    if ($wcz_telegram_debug == "1") 
+    {
+        file_put_contents(txpath."/tmp/telegram_request.txt","Sent URL: \n".$url."\n\nPrepared message:\n".$message."\n\nExcerpt length:\n".$excerptlen."\n\nCurl output:\n");
+    }
 
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 // some debug stuff
-	if ($wcz_telegram_debug == "1") 
-	{
-	    curl_setopt($ch, CURLOPT_VERBOSE, true);
-	    $curl_verbose = fopen(txpath.'/tmp/telegram_request.txt', 'a+');
-	    curl_setopt($ch, CURLOPT_STDERR, $curl_verbose);
-	}
+    if ($wcz_telegram_debug == "1") 
+    {
+        curl_setopt($ch, CURLOPT_VERBOSE, true);
+        $curl_verbose = fopen(txpath.'/tmp/telegram_request.txt', 'a+');
+        curl_setopt($ch, CURLOPT_STDERR, $curl_verbose);
+    }
 
-	$response = curl_exec($ch);
-	curl_close($ch);
+    $response = curl_exec($ch);
+    curl_close($ch);
 
     }
 }
-
-
-?>
